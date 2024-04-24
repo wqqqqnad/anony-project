8c7547d3bed16a5edf3058b9500e584d90cde376
JMW <wqqqqnad@gmail.comgit config --global user.email wqqqqnad@gmail.com>
2024-04-24 15:35:47 +0900

    upload apps

---
+++ apps/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app():
+
+    app=Flask(__name__)
+
+    from apps.crud import views as crud_views
+
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
---
+++ apps/crud/views.py
@@ -0,0 +1,14 @@
+from flask import Blueprint,render_template
+
+# Blueprint로 crud 앱을 생성한다
+crud = Blueprint(
+    "crud",
+    __name__,
+    template_folder="template",
+
+    static_folder="static",
+)
+
+@crud.route("/")
+def index():
+    return render_template("crud/index.html")
